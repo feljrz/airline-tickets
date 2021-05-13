9fb09af6afc007b689554d174a61fafc5e2a8030
Felipe <felipe.rezende149i@gmail.com>
2021-05-13 09:51:57 -0300

    Models and database connection

--- database/database.py
+++ database/database.py
@@ -1,15 +1,21 @@
+import sys, os
+sys.path.append(os.path.abspath(os.path.dirname(os.getcwd()+"/passagens_aereas"))) #Necessário durante o  desenvolvimento
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, scoped_session
 from sqlalchemy.ext.declarative import declarative_base
-
-import sys, os
-
-# sys.path.append(os.path.abspath(os.path.dirname(__file__)))
-sys.path.append(os.path.abspath(os.path.dirname(os.getcwd()+"/passagens_aereas")))
-for d in sys.path:
-    print(d)
-
 from settings.settings import DATABASE_URL
 
-x = 10
-# print(DATABASE_URL)
+engine = create_engine(DATABASE_URL, convert_unicode=True)
+db_session = scoped_session(sessionmaker(autocommit=False,
+                                         autoflush=False,
+                                         bind=engine))
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
+Base.query = db_session.query_property()
+
+def init_db():
+    from models import models
+    Base.metadata.create_all(bind=engine)
+
+init_db()
