6aa503be058061428e979ada40450d0f10d05972
Antonio Fernandes <fv.antonio@yahoo.com.br>
2021-05-18 11:25:29 -0300

    Pesquisa de voos por dia

---
+++ controllers/voo_controller.py
@@ -0,0 +1,12 @@
+from flask.globals import session
+from database import SessionLocal
+from models import Voo
+
+def hw_list_voos(date, company):
+    session = SessionLocal()
+    voos = session.query(Voo).filter_by(data=date, companhia=company).all()
+    list_voos = [{'id' : voo.id,
+                  'data' : voo.data,
+                  'destino' : voo.destino,} for voo in voos]
+    session.close()
+    return list_voos
--- routes.py
+++ routes.py
@@ -8,13 +8,21 @@ from controllers import *
 
 url_blueprint = Blueprint("urls", __name__)
 
+
 @url_blueprint.route("/aeroporto", methods=["POST"])
 def add_person():
     data = request.get_json()
     res = hw_add_aeroporto(data)
     return make_response(jsonify(res))
 
+
 @url_blueprint.route("/aeroporto", methods=['GET'])
 def list_aeroportos():
     res = hw_list_aeroporto()
+    return make_response(jsonify(res))
+
+@url_blueprint.route("/aeroporto/<day>/<month>/<company>", methods=['GET'])
+def get_voos(day,month,company):
+    date = day + '/' + month
+    res = hw_list_voos(date, company)
     return make_response(jsonify(res))
